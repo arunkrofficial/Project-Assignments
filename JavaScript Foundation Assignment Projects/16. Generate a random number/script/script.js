322b70b2d98408cb980372ada1e8ef977ddd1649
Arun Kumar <iamrun289@gmail.com>
2023-04-18 20:49:25 +0530

    Done!

--- JavaScript Foundation Assignment Projects/16. Generate a random number/script/script.js
+++
@@ -1,5 +0,0 @@
-addEventListener("load" , ()=>{
-	let randomNumber = (Math.floor(Math.random() * 100)) + 1 ;
-	document.querySelector("#op-screen").innerHTML = randomNumber ;
-})
-
--- JavaScript Foundation Assignment Projects/22. Progress Bar/script/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll" , function(){
-	let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  	let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  	let scrolled = (winScroll / height) * 100;
-  	document.getElementById("bar").style.width = scrolled + "%";
-})
